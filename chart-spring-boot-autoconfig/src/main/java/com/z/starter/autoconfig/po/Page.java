8e64e53be7422e160ada0a6949fa39a30f7ab2ac
zhaoxu1943 <463156880@qq.com>
2022-04-27 15:27:02 +0800

    fix: change cascade type for jpa
    cascade.all to cascade.merge

--- chart-spring-boot-autoconfig/src/main/java/com/z/starter/autoconfig/po/Page.java
+++ chart-spring-boot-autoconfig/src/main/java/com/z/starter/autoconfig/po/Page.java
@@ -30,7 +30,7 @@ public class Page extends BaseEntity implements Serializable {
     @Column()
     private String name;
 
-    @OneToMany(targetEntity = Card.class,cascade = CascadeType.ALL)
+    @OneToMany(targetEntity = Card.class,cascade = CascadeType.MERGE)
     @JoinTable(name = "plg_relation_page_card",
             joinColumns = {@JoinColumn(name = "page_id",referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name="card_id",referencedColumnName = "id")})
